4a45c9a6c4d22673f0d8abaaab37e3da24f3615b
SrJhol <mendes.jhonatan039@gmail.com>
2025-06-03 20:03:27 -0300

    Finalização do código JS

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ const pesquisarCep = async() => {
     if(cepValido(cep.value)){
         const dados = await fetch(url);
         const addres = await dados.json();
+
+        // console.log(addres);
         //hasOwnProperty retorna um valor boleano indicado se o objetivo possui a propriedade especifica
         if(addres.hasOwnProperty('erro')){
             alert("CEP não encontrado");
@@ -18,15 +20,22 @@ const pesquisarCep = async() => {
         } else{
             preencherFormulario(addres);
         }
+    
+    }else{
+        alert("CEP incorreto, tente novamente");
     }
 }
+const preencherFormulario = (endereco) => {
+    document.getElementById('rua').value = endereco.logradouro;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.cidade;
+    document.getElementById('estado').value = endereco.estado;
 
-preencherFormulario(endereco) => {
-    
 }
-limparFormulario = () => {
+const limparFormulario = () => {
     document.getElementById('rua').value = '';
     document.getElementById('bairro').value = '';
     document.getElementById('cidade').value = '';
     document.getElementById('estado').value = '';
 }
+document.getElementById('cep').addEventListener('focusout', pesquisarCep);
